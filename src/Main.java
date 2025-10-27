cafdf20c5c1711ccec4da9e320c8e5087cde1b77
A2kmoise <abayomoise950@gmail.com>
2025-10-27 21:34:24 +0200

    -{ * * }- initialised

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class Task{
+   private int id;
+   private String name;
+  private  Date createdAt;
+
+    Task(int id, String name, Date createdAt ){
+     this.id = id;
+     this.name = name;
+     this.createdAt = createdAt;
+    }
+}
+
+
+public  class Main{
+    public static void main(String [] args){
+        System.out.printf("%70s================Welcome to java TaskManager===============\n", "");
     }
 }
